139a4574639dd9a856c95c29a41e60a64f2fe498
RaptorialThing <ruzal.gabdrahmanoff@yandex.ru>
2021-11-16 20:22:57 +0300

    delete employers devise migrations

--- db/migrate/20211019135921_add_attribute_to_employers.rb
+++
@@ -1,6 +0,0 @@
-class AddAttributeToEmployers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :employers, :email,:string
-    add_column :employers, :password_digest, :string
-  end
-end
--- db/migrate/20211116154817_remove_email_from_employer.rb
+++
@@ -1,6 +0,0 @@
-class RemoveEmailFromEmployer < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :employers, :email, :string
-    remove_column :employers, :contact, :string
-  end
-end
